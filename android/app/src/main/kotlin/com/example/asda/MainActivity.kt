062d1a3caeffd67f37eda0a86d531de83b4dec44
Parabeac-Nest <admin@parabeac.com>
2022-11-09 20:49:36 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/asda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/asda_colors.g.dart
@@ -0,0 +1,42 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class AsdaColors {
+/// 1A1B1C
+static const Color lightTextColor = Color(0xff1b1e28);
+
+static const Color lightWhiteColor = Color(0xffffffff);
+/// 707B81
+static const Color lightSubTextColor = Color(0xff7d848d);
+/// 5B9EE1
+static const Color lightPrimaryColor = Color(0xff0d6efd);
+
+static const Color lightStarColor = Color(0xffffd336);
+/// FF5545
+static const Color lightActionColor = Color(0xffff7029);
+/// F8F9FA
+static const Color lightFrameShapeColor = Color(0xffe5f4ff);
+
+static const Color lightGrayColor = Color(0xfff7f7f9);
+
+static const Color lightRedColor = Color(0xfffe2352);
+
+static const Color lightInk = Color(0xff000000);
+
+static const Color lightPrimary = Color(0xffffffff);
+
+static const Color lightSecondary = Color(0xffaeb3be);
+
+static const Color darkInk = Color(0xffffffff);
+/// #FFFFFF
+static const Color lightBGColor = Color(0xffffffff);
+
+static const Color lightGreenColor = Color(0xff19b000);
+
+static const Color dark = Color(0xff28303f);
+}
